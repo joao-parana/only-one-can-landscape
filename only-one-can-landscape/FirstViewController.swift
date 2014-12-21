3b8ca5a4a013d918849d60095ebf5c739143006d
João Antonio Ferreira <joao.parana@gmal.com>
2014-12-21 21:57:34 -0200

    second commit

---
+++ only-one-can-landscape/FirstViewController.swift
@@ -0,0 +1,26 @@
+//
+//  FirstViewController.swift
+//  only-one-can-landscape
+//
+//  Created by Joao Ferreira on 12/21/14.
+//  Copyright (c) 2014 si. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        println("First loaded")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ only-one-can-landscape/SecondViewController.swift
@@ -0,0 +1,26 @@
+//
+//  SecondViewController.swift
+//  only-one-can-landscape
+//
+//  Created by Joao Ferreira on 12/21/14.
+//  Copyright (c) 2014 si. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        println("Second loaded")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
